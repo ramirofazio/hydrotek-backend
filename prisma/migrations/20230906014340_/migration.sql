0cc016370c45084d684d430cea5dbf1b990dbb14
Ramiro Fazio Dattoli <70992145+ramirofazio@users.noreply.github.com>
2023-09-12 19:14:10 -0300

    Delete prisma/migrations directory

--- prisma/migrations/20230906014340_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Role" ALTER COLUMN "type" DROP DEFAULT;
--- prisma/migrations/20230910213840_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `updated` to the `Product` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "updated" TEXT NOT NULL,
-ALTER COLUMN "description" DROP NOT NULL;
