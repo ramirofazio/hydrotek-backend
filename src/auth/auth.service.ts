bc6d7a59f179845a2f3d59573b71bed412d0350d
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-09-26 21:36:42 -0300

    add auto login with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,6 +13,7 @@ import { OAuth2Client } from "google-auth-library";
 import { env } from "process";
 import axios from "axios";
 
+
 @Injectable()
 export class AuthService {
   /* eslint-disable */
@@ -139,14 +140,28 @@ export class AuthService {
       };
     }
   }
-  async jwtAutoLogin(accessToken: string): Promise<any> {
-    /* const j = this.jwtService.verifyAsync(accessToken, {
-      secret: env.JWT_SECRET_KEY,
-    });
-    console.log(j) */
-    const { sub, name } = this.jwtService.decode(accessToken);
-    const userInfo = await this.prisma.user.findUnique(sub);
-    const res = await this.userServices.findByEmail(userInfo.email); // Es sehuro?, incluir email en jwt
-    return this.jwtService.decode(accessToken);
+  async jwtAutoLogin(accessToken: string): Promise<UserSignInResponseDTO2> {
+    try {
+      console.log("se intento con el JWT: ", accessToken);
+      const { sub } = await this.jwtService.verifyAsync(accessToken, {
+        secret: env.JWT_SECRET_KEY,
+      });
+      const { email } = await this.prisma.user.findUnique({
+        where: { id: sub },
+      });
+      const userInfo = await this.userServices.findByEmail(email);
+      const { id, name, role } = userInfo;
+      const payload = { sub: id, name: name, role: role.type };
+
+      return {
+        session: { id: id, email: userInfo.email, role: role.type },
+        profile: userInfo.profile,
+        shoppingCart: userInfo.shoppingCart,
+        accessToken: await this.jwtService.signAsync(payload),
+      };
+
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.REQUEST_TIMEOUT);
+    }
   }
 }
