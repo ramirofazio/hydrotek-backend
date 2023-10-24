746a3ffefabf4f282f9e6595d5079d2be5534359
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-10-24 01:01:23 -0300

    a

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -12,26 +12,30 @@ export class CloudinaryService {
   /* eslint-enable */
 
   getSignature() {
-    /* const timestamp = Math.round(new Date().getTime() / 1000);
+    const timestamp = Math.round(new Date().getTime() / 1000);
     console.log(env.CLOUDINARY_API_SECRET);
     const signature = cloudinary.utils.api_sign_request(
       {
         timestamp: timestamp,
+        upload_preset: "user_avatar",
         public_id: "cacatua",
-        eager: "w_400,h_300,c_pad|w_260,h_200,c_crop",
+        api_key: env.CLOUDINARY_API_KEY,
+        api_secret: env.CLOUDINARY_API_SECRET,
+        cloud_name: env.CLOUDINARY_CLOUD_NAME,
+        overwrite: true,
         //upload_preset: "user_avatar",
       },
       `${env.CLOUDINARY_API_SECRET}`
     );
     console.log("im in");
-    console.log(signature); */
-    return { timestamp: "", signature: "" };
+    console.log(signature);
+    return { timestamp, signature};
   }
 
   async updateAvatar(file: any) {
     try {
       const { timestamp, signature } = this.getSignature();
-      const result = await cloudinary.uploader.upload("./testBorder.jpg", {
+      const result = await cloudinary.uploader.upload(file, {
         upload_preset: "user_avatar",
         public_id: "cacatua",
         api_key: env.CLOUDINARY_API_KEY,
