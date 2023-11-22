fed915aefa90c60f915ca2480eb90686a42e3ee5
josesantangelo <josesantangelo93@gmail.com>
2023-11-22 01:29:27 -0300

    Se agregan endpoints para generar checkout a invitados o a usuarios sin dni cargado previamente

---
+++ src/mobbex/mobbex.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { MobbexService } from "./mobbex.service";
+import { mobbex } from "mobbex";
+import { CheckoutGuestRequest, CheckoutRequest } from "./mobbex.dto";
+
+
+@Controller("mobbex")
+export class MobbexController {
+  // eslint-disable-next-line no-unused-vars
+  constructor(private readonly mobbexService: MobbexService) {}
+
+  @Post()
+  async generateCheckout(@Body() body: CheckoutRequest) {
+    try {
+      const mobbexBody = await this.mobbexService.generateBody(body.userId, body.items);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const checkout: any = await mobbex.checkout.create(mobbexBody);
+      if("data" in checkout) {
+        return checkout.data.url;
+      }
+      if("error" in checkout) {
+        throw checkout.error;
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  @Post("update-dni")
+  async updateUserAndGenerateCheckout(@Body() body: CheckoutRequest) {
+    try {
+      await this.mobbexService.updateUser(body.userId, body.identifier);
+      await this.generateCheckout(body);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  @Post("guest")
+  async generateGuestCheckout(@Body() body: CheckoutGuestRequest) {
+    try {
+      const mobbexBody = await this.mobbexService.generateGuestBody(body);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const checkout: any = await mobbex.checkout.create(mobbexBody);
+      if("data" in checkout) {
+        return checkout.data.url;
+      }
+      if("error" in checkout) {
+        throw checkout.error;
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
---
+++ src/mobbex/mobbex.dto.ts
@@ -0,0 +1,126 @@
+import { Type } from "class-transformer";
+import {
+  IsNotEmpty,
+  IsString,
+  IsArray,
+  ArrayMinSize,
+  ValidateNested,
+  IsNumber,
+  IsOptional,
+} from "class-validator";
+
+export class CheckoutRequest {
+  @IsString()
+  @IsOptional()
+    identifier: string;
+  @IsString()
+  @IsNotEmpty()
+    userId: string;
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => requestItem)
+    items: requestItem[];
+}
+
+export class CheckoutGuestRequest {
+  @IsString()
+  @IsNotEmpty()
+    firstName: string;
+  @IsString()
+  @IsNotEmpty()
+    lastName: string;
+  @IsString()
+  @IsNotEmpty()
+    email: string;
+  @IsString()
+  @IsOptional()
+    dni: string;
+  @IsString()
+  @IsOptional()
+    phone: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => requestItem)
+    items: requestItem[];
+}
+
+export class requestItem {
+  @IsNotEmpty()
+  @IsNumber()
+    id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+    qty: number;
+}
+
+export interface mobbexCustomer {
+  email: string;
+  name: string;
+  identification: string;
+  uid: string;
+  phone?: string;
+}
+
+
+export interface mobbexGuestCustomer {
+  email: string;
+  name: string;
+  identification?: string;
+  phone?: string;
+}
+
+
+
+export interface mobbexItem {
+  description: string;
+  quantity: number;
+  total: number;
+  image: string;
+}
+
+export interface mobbexBody {
+  total: number;
+  description: string;
+  reference: string;
+  currency: string;
+  test: boolean;
+  return_url: string;
+  customer: mobbexCustomer | mobbexGuestCustomer;
+  items: mobbexItem[];
+}
+
+export interface successCheckoutResponse {
+  result: true;
+  data: successDataCheckoutResponse;
+}
+
+export interface failureCheckoutResponse {
+  result: false;
+  code: string;
+  error: string;
+}
+
+interface successDataCheckoutResponse {
+  id: string;
+  url: string;
+  description: string;
+  currency: string;
+  total: number;
+  timeout: number;
+  created: number;
+  paymentMethods: paymentMethod[];
+}
+
+interface paymentMethod {
+  group: string;
+  subgroup: string;
+  subgroup_title: string;
+  subgroup_logo: string;
+  type: string;
+}
