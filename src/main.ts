8d7aae7c4098debc59dae76fe89659ce630e48cc
Your Name <ramifazio@gmail.com>
2023-11-28 20:21:57 -0300

    add mobbex config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import type { NestExpressApplication } from "@nestjs/platform-express";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { RoleService } from "./role/role.service";
 import { env } from "process";
+import mobbex from "mobbex";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,7 +15,6 @@ async function bootstrap() {
     origin: env.ACCESS_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-
   });
   app.useBodyParser("json", { limit: "20mb" });
   const config = new DocumentBuilder()
@@ -38,6 +38,10 @@ async function bootstrap() {
       },
     })
   );
+  mobbex.configurations.configure({
+    apiKey: process.env.MOBBEX_X_API_KEY,
+    accessToken: process.env.MOBBEX_X_ACCESS_TOKEN,
+  });
   await app.listen(3000);
 }
 bootstrap();
