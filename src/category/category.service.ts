3788a9af4e8c44cf18ebab0e7a3a180c42400e4f
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-12-04 21:39:49 -0300

    add categorias

---
+++ src/category/category.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class CategoryService {
+  /* eslint-disable */
+  constructor(private prisma: PrismaService) {}
+  /* eslint-enable */
+
+  async createCategories() {
+    const categories = ["Sistemas", "insumos", "fertilizantes", "aditivos"];
+    const res = categories.map(async (c) => {
+      const exist = await this.prisma.productType.findFirst({
+        where: { type: c },
+      });
+      if (!exist) {
+        await this.prisma.productType.create({
+          data: {
+            type: c,
+          },
+        });
+      }
+    });
+
+    return res;
+  }
+}
