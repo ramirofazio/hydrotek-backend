77f117bc0f7829461696b6a5395979299a8dd856
Ramiro Fazio Dattoli <70992145+ramirofazio@users.noreply.github.com>
2024-05-16 16:35:20 -0300

    Master (#55)
    * final fixes
    
    * add migrations
    
    * add more time to jwt
    
    * add: start.sh
    
    * add migrations history
    
    * restore migrations
    
    * Update compose
    
    * update compose
    
    * update compose
    
    * update compose
    
    * update compose
    
    * update compose
    
    * update gitIgnore

---
+++ prisma/migrations/20240504173633_/migration.sql
@@ -0,0 +1,330 @@
+-- CreateEnum
+CREATE TYPE "AssetType" AS ENUM ('IMAGE', 'VIDEO');
+
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "value" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TestCategory" (
+    "id" SERIAL NOT NULL,
+    "value" INTEGER NOT NULL,
+
+    CONSTRAINT "TestCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "tFacturaId" INTEGER,
+    "name" TEXT NOT NULL,
+    "dni" INTEGER,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "roleId" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserProfile" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "avatar" TEXT DEFAULT 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=',
+    "address" TEXT,
+    "province" TEXT,
+    "city" TEXT,
+    "postalCode" INTEGER,
+
+    CONSTRAINT "UserProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShoppingCart" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "totalPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "ShoppingCart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductsOnCart" (
+    "shoppingCartId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" INTEGER,
+    "name" TEXT,
+    "img" TEXT,
+
+    CONSTRAINT "ProductsOnCart_pkey" PRIMARY KEY ("shoppingCartId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "publishDate" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PostAsset" (
+    "id" SERIAL NOT NULL,
+    "type" "AssetType" DEFAULT 'IMAGE',
+    "path" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+
+    CONSTRAINT "PostAsset_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PostComment" (
+    "id" SERIAL NOT NULL,
+    "publishDate" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "comment" TEXT NOT NULL,
+    "show" BOOLEAN DEFAULT false,
+    "postId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "PostComment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SavedPost" (
+    "id" SERIAL NOT NULL,
+    "postId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "SavedPost_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "validUntil" TEXT NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "usdPrice" DOUBLE PRECISION NOT NULL,
+    "arsPrice" DOUBLE PRECISION NOT NULL,
+    "description" TEXT,
+    "published" BOOLEAN NOT NULL,
+    "type" INTEGER NOT NULL,
+    "profile" INTEGER NOT NULL,
+    "featured" BOOLEAN NOT NULL DEFAULT false,
+    "updated" TEXT NOT NULL,
+    "typeId" INTEGER
+);
+
+-- CreateTable
+CREATE TABLE "ProductType" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "ProductType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductImage" (
+    "id" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "publicId" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "index" INTEGER,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PromotionalCode" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "discount" INTEGER NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "PromotionalCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PromotionalCodeOnProducts" (
+    "productId" INTEGER NOT NULL,
+    "promotionalCodeId" TEXT NOT NULL,
+
+    CONSTRAINT "PromotionalCodeOnProducts_pkey" PRIMARY KEY ("productId","promotionalCodeId")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT,
+    "email" TEXT,
+    "name" TEXT,
+    "totalPrice" INTEGER NOT NULL,
+    "discount" INTEGER NOT NULL DEFAULT 0,
+    "fresaId" TEXT NOT NULL,
+    "status" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProducts" (
+    "id" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderProducts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TokenLog" (
+    "id" SERIAL NOT NULL,
+    "errorCode" INTEGER,
+    "data" TEXT,
+    "date" TEXT NOT NULL,
+
+    CONSTRAINT "TokenLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductLog" (
+    "id" SERIAL NOT NULL,
+    "errorCode" INTEGER,
+    "data" TEXT,
+    "date" TEXT NOT NULL,
+
+    CONSTRAINT "ProductLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TFacturaUserLog" (
+    "id" SERIAL NOT NULL,
+    "errorCode" INTEGER,
+    "data" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+
+    CONSTRAINT "TFacturaUserLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DollarPrice" (
+    "id" SERIAL NOT NULL,
+    "date" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "DollarPrice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_tFacturaId_key" ON "User"("tFacturaId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_dni_key" ON "User"("dni");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProfile_userId_key" ON "UserProfile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ShoppingCart_userId_key" ON "ShoppingCart"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_id_key" ON "Product"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PromotionalCode_code_key" ON "PromotionalCode"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Order_fresaId_key" ON "Order"("fresaId");
+
+-- AddForeignKey
+ALTER TABLE "Test" ADD CONSTRAINT "Test_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "TestCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserProfile" ADD CONSTRAINT "UserProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShoppingCart" ADD CONSTRAINT "ShoppingCart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductsOnCart" ADD CONSTRAINT "ProductsOnCart_shoppingCartId_fkey" FOREIGN KEY ("shoppingCartId") REFERENCES "ShoppingCart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductsOnCart" ADD CONSTRAINT "ProductsOnCart_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostAsset" ADD CONSTRAINT "PostAsset_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostComment" ADD CONSTRAINT "PostComment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostComment" ADD CONSTRAINT "PostComment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SavedPost" ADD CONSTRAINT "SavedPost_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SavedPost" ADD CONSTRAINT "SavedPost_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "ProductType"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PromotionalCodeOnProducts" ADD CONSTRAINT "PromotionalCodeOnProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PromotionalCodeOnProducts" ADD CONSTRAINT "PromotionalCodeOnProducts_promotionalCodeId_fkey" FOREIGN KEY ("promotionalCodeId") REFERENCES "PromotionalCode"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
